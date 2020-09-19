172b3c4c566fd93d08046add248c803097a92ca3
Castle Lemongrab <67447858+castle-lemongrab@users.noreply.github.com>
2020-09-19 12:32:24 -0700

    It almost seems to work

--- src/tinywire.sh
+++ src/tinywire.sh
@@ -72,7 +72,7 @@ install_apt() {
   apt-get -q update &&
   apt-get -q -y -o Dpkg::Options::="--force-confnew" dist-upgrade &&
   apt-get -q -y -o Dpkg::Options::="--force-confnew" install \
-    wireguard wireguard-dkms zlib1g-dev uuid-dev libblkid-dev nodejs \
+    ipcalc wireguard wireguard-dkms zlib1g-dev uuid-dev libblkid-dev \
     daemontools-run ucspi-tcp exiftool jq curl qrencode iputils-ping &&
   \
   apt-get -q -y autoremove &&
@@ -108,6 +108,77 @@ deploy_dnscache() {
   ln -s /etc/dnscache /etc/service/dnscache
 }
 
+deploy_wireguard() {
+
+  local POSTUP="iptables -A FORWARD -i %i -j ACCEPT;"
+  POSTUP="$POSTUP iptables -A FORWARD -o %i -j ACCEPT;"
+  POSTUP="$POSTUP iptables -t nat -A POSTROUTING -o $IF -j MASQUERADE"
+
+  local POSTDOWN="iptables -D FORWARD -i %i -j ACCEPT;"
+  POSTDOWN="$POSTDOWN iptables -D FORWARD -o %i -j ACCEPT;"
+  POSTDOWN="$POSTDOWN iptables -t nat -D POSTROUTING -o $IF -j MASQUERADE"
+
+  umask 0277 &&
+  wg genkey | ./bin/iniedit add -f /dev/null -s Interface \
+    -l "ListenPort = $PORT" -l 'PrivateKey' \
+    -l "Address = $WIREGUARD_IP/$WIREGUARD_NETMASK" \
+    -l "PostUp = $POSTUP" -l "PostDown = $POSTDOWN" > "$WIREGUARD_CONFIG"
+}
+
+wireguard_add_peer()
+{
+  local name="`basename "$1"`"
+  local ip="$2"
+
+  if [ -z "$name" ]; then
+    return 1
+  fi
+
+  if [ -z "$ip" ]; then
+    return 2
+  fi
+
+  umask 0277 &&
+  trap "rm -f '$key_tmp' '$server_config_tmp' '$peer_config_tmp'" EXIT &&
+  \
+  local key_tmp="`tempfile -m 0400`" &&
+  local peer_config_tmp="`tempfile -m 0400 -d temp -s .ini`" &&
+  local server_config_tmp="`tempfile -m 0400 -d temp -s .ini`" &&
+  \
+  # Peer private key
+  wg genkey > "$key_tmp" &&
+  \
+  # Modify server configuration
+  wg pubkey < "$key_tmp" | ./bin/iniedit add \
+    -f "$WIREGUARD_CONFIG" -s Peer -c "$name" \
+    -l PublicKey -l AllowedIPs="$ip/32" > "$server_config_tmp" && \
+  \
+  # Create peer configuration file
+  (./bin/iniedit add -f /dev/null \
+     -s Interface -c "$name" -l "Address = $2" \
+       -l "DNS = $WIREGUARD_DNS_IP" -l PrivateKey < "$key_tmp" &&
+   ./bin/iniedit read -f "$WIREGUARD_CONFIG" \
+     -s Interface -l PrivateKey | wg pubkey | \
+   ./bin/iniedit add -f /dev/null \
+     -s Peer -l "Endpoint = $DOMAIN:$PORT" \
+     -l 'AllowedIPs = 0.0.0.0/0, ::/0' \
+     -l 'PersistentKeepalive=60' -l PublicKey) > "$peer_config_tmp"
+
+  if [ "$?" -eq 0 ]; then
+    # Print peer configuration
+    echo && qrencode -t ansiutf8 < "$peer_config_tmp" &&
+    echo && cat "$peer_config_tmp" &&
+    \
+    # Commit server changes
+    mv "$server_config_tmp" "$WIREGUARD_CONFIG" &&
+    wg addconf wg0 <(wg-quick strip wg0)
+  fi
+
+  local rv="$?"
+  rm -f "$key_tmp" "$server_config_tmp" "$peer_config_tmp"
+  return "$rv"
+}
+
 install_all () {
 
   log install_pv &&
@@ -116,6 +187,7 @@ install_all () {
   log install_nvm &&
   log install_node &&
   log install_iniedit &&
+  log deploy_wireguard &&
   log install_djbdns &&
   log deploy_dnscache &&
   \
